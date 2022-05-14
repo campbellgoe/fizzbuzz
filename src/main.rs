165af2ff37176e802509de16bc1f471cf18cdcc7
George Campbell <campbell.goe@gmail.com>
2022-05-14 22:36:32 +0100

    third way to generate FizzBuzz

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 fn main() {
-    fizzbuzz_a();
-    fizzbuzz_b();
+    // fizzbuzz_a();
+    // fizzbuzz_b();
+    _fizzbuzz_c();
 }
 
 // A way to generate fizzbuzz by concatenating Fizz and Buzz based on divisibility by 3 and 5
-fn fizzbuzz_a() {
+fn _fizzbuzz_a() {
     for i in 0..100 {
         let mut out = String::from("");
         let div3 = i % 3 == 0;
@@ -24,9 +25,9 @@ fn fizzbuzz_a() {
 }
 
 // Another way to generate fizzbuzz by explicitly handling each kind of output
-fn fizzbuzz_b() {
+fn _fizzbuzz_b() {
     for i in 0..100 {
-        if i % 3 == 0 && i % 5 == 0 {
+        if i % 15 == 0 {
             println!("FizzBuzz");
         } else if i % 3 == 0 {
             println!("Fizz");
@@ -36,4 +37,22 @@ fn fizzbuzz_b() {
             println!("{}", i);
         }
     }
+}
+
+// Another way to generate FizzBuzz using match
+fn _fizzbuzz_c() {
+
+    for i in 0..100 {
+        let num_string;
+        let out = match i {
+            i if i % 15 == 0 => { "FizzBuzz" }
+            i if i % 3 == 0 => { "Fizz" }
+            i if i % 5 == 0 => { "Buzz" }
+            _ => {
+                num_string = i.to_string();
+                &num_string
+            }
+        };
+        println!("{}", out);
+    };
 }
